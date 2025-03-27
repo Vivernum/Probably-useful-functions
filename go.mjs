5f42fbf36259a1e129e596d1905494ce86fb0654
Vivernum <146701606+Vivernum@users.noreply.github.com>
2025-03-27 22:12:16 +0700

    Update go.mjs
    just did it so elemet now won't leave the screen (tested only with widdth and height are set in 'px'

--- go.mjs
+++ go.mjs
@@ -1,12 +1,20 @@
-export function RunAway (block) {
+export function RunAway (element) {
 
-    block.addEventListener('mouseover', function() {
-        let h = Math.floor(Math.random() * document.documentElement.clientHeight); // gets data about screen dimensions
-        let w = Math.floor(Math.random() * document.documentElement.clientWidth);  // and converts to random coordinates
+    element.addEventListener('mouseover', function() {
+        let h =  Math.floor(Math.random() * (document.documentElement.clientHeight - element.clientHeight)); 
+        let w =  Math.floor(Math.random() * (document.documentElement.clientWidth - element.clientWidth)); 
+        // gets client's window width and height, and then subtracts element's 'w' and 'h'
+        // (so that the element won't leave the screen)
 
-        block.style.position = 'fixed';
-        block.style.bottom = h + 'px';
-        block.style.right = w + 'px';
+        // then just randomises the result  
+
+
+        element.style.position = 'fixed';
+        element.style.bottom = h + 'px';
+        element.style.right = w + 'px';
     })
-} // long story short - makes your button escape the mouse 
-// obviously can be used with other objects, but i did't test it ;)
+}   
+
+    // long story short - makes your button escape the mouse 
+    // obviously can be used with other objects, but i did't test it ;)
+    // didn't test it with anything other than px's though :)
